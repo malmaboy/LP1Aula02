6078b78d1a0932325b5828f5f4dbcd6775f6292f
Hugo Carvalho <huguinho66@live.com>
2020-03-05 16:28:09 +0000

    A toxic conversation with a bot

---
+++ ChatBot/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ChatBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variaveis
+            string response;
+
+            do
+            {
+                response = Console.ReadLine();
+
+
+                //Verifica a resposta
+
+                switch(response)
+                {
+                    case "How are you?":
+                        Console.WriteLine("im good bro, and U?");
+                        break;
+                    case "Are u having fun?":
+                        Console.WriteLine("Nah.");
+                        break;
+                    case "EXIT":
+                        Console.WriteLine("Goobye bro.");
+                        System.Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("WTF??");
+                        break;
+                }
+            
+            } while (true);
+
+        }   
+    }
+}
