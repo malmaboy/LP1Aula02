55b82d176ba797658361d3259c897904bac54479
Hugo Carvalho <huguinho66@live.com>
2020-03-05 09:33:42 +0000

    Operadores de Incremento e Decremento

---
+++ IncDec/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IncDec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = -5;
+
+            int c = a++ + b++;
+
+            Console.WriteLine(c);
+
+            int d = ++a + ++b;
+
+            Console.WriteLine(d);
+
+        }
+    }
+}
