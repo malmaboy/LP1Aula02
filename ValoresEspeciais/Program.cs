6dc64dcdab25a504fd7977882bf1e107c95c70f3
Hugo Carvalho <huguinho66@live.com>
2020-03-05 11:44:51 +0000

    Exercicio 6 Valores especiais

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            Console.WriteLine($"{double.MinValue}");
+            Console.WriteLine($"{double.MaxValue}");
+
+            Console.WriteLine($"{int.MaxValue}");
+            Console.WriteLine($"{int.MinValue}");
+
+            Console.WriteLine($"{float.MaxValue}");
+            Console.WriteLine($"{float.MinValue}");
+
+            Console.WriteLine($"{long.MaxValue}");
+            Console.WriteLine($"{long.MinValue}");
+
+            Console.WriteLine($"{double.PositiveInfinity}");
+            Console.WriteLine($"{double.NegativeInfinity}");
+
+            Console.WriteLine($"{float.NegativeInfinity}");
+            Console.WriteLine($"{float.NegativeInfinity}");
+
+        }
+    }
+}
