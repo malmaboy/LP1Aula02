9817a2e588504f3d0b21ceb993d5026420e14ee9
Hugo Carvalho <huguinho66@live.com>
2020-03-05 12:16:31 +0000

    Exercicio 6 Valores especiais com over and under

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -6,7 +6,8 @@ namespace ValoresEspeciais
     {
         static void Main(string[] args)
         {
-        
+            // Valores maximos e minimos
+            /*
             Console.WriteLine($"{double.MinValue}");
             Console.WriteLine($"{double.MaxValue}");
 
@@ -19,11 +20,36 @@ namespace ValoresEspeciais
             Console.WriteLine($"{long.MaxValue}");
             Console.WriteLine($"{long.MinValue}");
 
+            // Valores infinitos
             Console.WriteLine($"{double.PositiveInfinity}");
             Console.WriteLine($"{double.NegativeInfinity}");
 
             Console.WriteLine($"{float.NegativeInfinity}");
             Console.WriteLine($"{float.NegativeInfinity}");
+            */
+
+            // Overflow com doubles e floats
+
+            double x = 2 * double.MaxValue;
+            double p = double.MaxValue + 1;
+
+            float w = 2 * float.MaxValue;
+            float g = float.MaxValue + 1;
+
+            Console.WriteLine($"{x}");
+            Console.WriteLine($"{p}");
+            Console.WriteLine($"{w}");
+            Console.WriteLine($"{g}");
+
+            // Underflow com float e double
+
+            float f1 = 10000000000000000;
+            float f2 = float.MinValue;
+            double g1 = double.MinValue;
+            double g2 = double.MaxValue;
+
+            Console.WriteLine(f1 + 0.00000001f);
+            Console.WriteLine(g2 * 2000000000);
 
         }
     }
